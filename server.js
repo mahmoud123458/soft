cdf12644aef7e2675b56e2905c4c314cf4f34b6d
mahmoud123458 <mahmoudnabil11112@gmail.com>
2023-05-10 15:27:51 +0300

    Addes dotenv

---
+++ server.js
@@ -0,0 +1,20 @@
+import express from 'express';
+import { engine } from 'express-handlebars';
+import dotenv from 'dotenv';
+dotenv.config();
+import mongoose from 'mongoose';
+
+
+mongoose.connect(process.env.mongoConnectionUrl)
+
+const app =express();
+
+
+app.engine('handlebars', engine());
+app.set('view engine', 'handlebars');
+app.set('views', './templates');
+
+
+app.listen(process.env.port,() =>{
+    console.log(`Started the application on http://localhost:${process.env.port}`);
+});
